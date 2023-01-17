a8385610299297dcddef8e5070d9435c78a65af9
RatedYK <terrence0101@yahoo.com>
2023-01-17 19:03:20 +0900

    create basic skeleton html and css + modules for task and project ToDo: make functioning buttons

---
+++ src/modules/project.js
@@ -0,0 +1,18 @@
+export default class Project {
+    constructor(title) {
+        this.title = title;
+        this.tasks = [];
+    }
+    getTask() {
+        return this.tasks;
+    }
+    setTask(tasks) {
+        this.tasks = tasks;
+    }
+    addTask(newTask) {
+        this.tasks.push(newTask);
+    }
+    deleteTask(taskTitle) {
+        this.tasks = this.tasks.filter((task) => {task.title !== taskTitle});
+    }
+}
